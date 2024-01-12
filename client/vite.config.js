9acf8d74840b14709ffa64d9fcc466279d7b13ac
Rahul Thakur <rahul_thakur@Rahuls-MacBook-Air.local>
2024-01-12 12:26:05 +0530

    asdf

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/  
 export default defineConfig({
-  server:{
-    proxy:{
-      // '/api': 'http://localhost:3000'   can't use this becuase it's not secure each time you /api add http://localhost:3000 at beginning
-      '/api':{
+  server: {
+    proxy: {
+      '/api': {
         target: 'https://dream-estate-vercel-api-eight.vercel.app',
-        secure:false,
+        secure: false,
       },
     },
   },
   plugins: [react()],
-})
+});
