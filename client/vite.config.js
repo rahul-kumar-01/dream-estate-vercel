b3d70de5902f9bc45bc691b174864e2697d7593c
Rahul Thakur <rahul_thakur@Rahuls-MacBook-Air.local>
2024-01-12 13:17:29 +0530

    final today commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';  // Correct import for React support
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],  // Re-enable for React
+  plugins: [react()],
   server: {
     proxy: {
       '/api': {
         target: 'https://dream-estate-vercel-api-eight.vercel.app',
-        secure: false,  
         changeOrigin: true,
       },
     },
   },
 });
+
 
