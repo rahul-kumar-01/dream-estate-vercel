100002e0ddd8a927ed7d7a3ff3b4683dbd83b3a3
Rahul Thakur <rahul_thakur@Rahuls-MacBook-Air.local>
2024-01-13 02:16:47 +0530

    sdf

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -25,14 +25,14 @@ export default function SignIn() {
     try{
 
         dispatch(signInStart());
-      const res = await fetch('http://localhost:3000/api/auth/signin',
+      const res = await fetch('https://dream-estate-vercel-api-eight.vercel.app/api/auth/signin',
       {
         method: 'POST',
         headers: {
           'Content-Type' : 'application/json',
         },
         body: JSON.stringify(formData),
-        credrentials: 'include',
+        credentials: 'include',
       });
       const data = await res.json();
 
