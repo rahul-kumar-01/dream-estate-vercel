bf020f3eaa3d49b4405848db7d4461c31a2d042a
Rahul Thakur <rahul_thakur@Rahuls-MacBook-Air.local>
2024-01-13 02:21:25 +0530

    asdf

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -62,7 +62,7 @@ export default function Profile() {
     event.preventDefault();
     try{
         dispatch(updateUserStart());
-        const res = await fetch(`https://dream-estate-vercel.vercel.app/api/user/update/${currentUser._id}`, {
+        const res = await fetch(`https://dream-estate-vercel-api-eight.vercel.app/api/user/update/${currentUser._id}`, {
           method: 'POST',
           headers:{
             'Content-Type': 'application/json',
