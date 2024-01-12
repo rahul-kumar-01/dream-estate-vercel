3894ee378399e624bd13036f621292446603b661
Rahul Thakur <rahul_thakur@Rahuls-MacBook-Air.local>
2024-01-12 21:41:47 +0530

    asdf

--- api/index.js
+++ api/index.js
@@ -40,9 +40,18 @@ app.use(express.json()); // allow json as input to the server (req)
 app.use(cookieParser());
 
 
+
 app.listen(3000,()=>{
     console.log('Server is running');
 })
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Origin', 'https://dream-estate-vercel.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    next();
+  });
 
 
 app.get('/test',(req,res)=>{
