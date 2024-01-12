f0016ad842579f8e885dd1ef3ead376e1bc1673a
Rahul Thakur <rahul_thakur@Rahuls-MacBook-Air.local>
2024-01-13 01:36:17 +0530

    asdf

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -25,7 +25,7 @@ export default function SignIn() {
     try{
 
         dispatch(signInStart());
-      const res = await fetch('https://dream-estate-vercel.vercel.app/api/auth/signin',
+      const res = await fetch('https://dream-estate-vercel-api-eight.vercel.app/api/auth/signin',
       {
         method: 'POST',
         headers: {
@@ -41,7 +41,6 @@ export default function SignIn() {
         return;
       }
       const token = data.token;
-
       dispatch(signInSuccess(data.rest));
       
 
