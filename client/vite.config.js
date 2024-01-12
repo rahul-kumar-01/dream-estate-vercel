e19ad72bbd3afab0d64cc494d02a52dca9b86d93
Rahul Thakur <rahul_thakur@Rahuls-MacBook-Air.local>
2024-01-12 13:10:35 +0530

    asdfasdfasdf

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'https://dream-estate-vercel-api-eight.vercel.app',
-        secure: true,  
+        secure: false,  
         changeOrigin: true,
       },
     },
