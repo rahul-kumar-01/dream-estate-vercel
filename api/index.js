278117f19a37cfc684a58a8ba9b6c21f351102e6
Rahul Thakur <rahul_thakur@Rahuls-MacBook-Air.local>
2024-01-13 14:45:01 +0530

    sdfdsf

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const __dirname = path.resolve();
 const app = express();
 
 app.use(cors({
-  origin: ['https://dream-estate.vercel.app','http://localhost:5173/'],
+  origin: ['https://dream-estate.vercel.app','http://localhost:5173'],
   credentials: true,
 }));
 
